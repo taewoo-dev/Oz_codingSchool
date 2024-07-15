1831463cbafda6f1a26fff321229894953a2f62d
taewoo-dev <twin9774@gmail.com>
2024-07-15 12:01:38 +0900

    study_stack

---
+++ 자료구조및알고리즘/stack.py
@@ -0,0 +1,37 @@
+class Stack:
+    def __init__(self) -> None:
+        self._items = [] # 데이터 저장을 위한 빈 리스트 생성
+
+    def push(self,val):
+        self._items.append(val) # 단순 대입연산으로 단위 시간연산
+
+    def pop(self):
+        try:
+            return self._items.pop() # 단순 제거 연산으로 단위 시간연산
+        except IndexError:
+            print("Stack is Empty")
+    
+    def top(self):
+        try:
+            return self._items[-1] # 단순 참조 단위시간 연산
+        except IndexError:
+            print("Stack is Empty")
+
+    def __len__(self):
+        return len(self._items)
+    
+arraystack = Stack()
+
+            
+from stack import Stack
+
+arraystack = Stack()
+
+for i in "HELLO":
+    arraystack.push(i)
+
+print("".join(arraystack._items))
+print(arraystack.top())
+print(arraystack.pop())
+print(arraystack._items)
+print(arraystack.top())
