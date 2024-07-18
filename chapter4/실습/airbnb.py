b1adc46ba4491beef65f2dd346ac6cbb4d96534f
taewoo-dev <twin9774@gmail.com>
2024-07-18 16:53:14 +0900

    ----

--- chapter4/실습/airbnb.py
+++ chapter4/실습/airbnb.py
@@ -26,7 +26,7 @@ def main():
     conn = pymysql.connect(
                             host="localhost",
                             user="root",
-                            password="1520528a",
+                            password="--------",
                             database="airbnb_db",
                             charset="utf8mb4",
                             cursorclass=pymysql.cursors.DictCursor # json 형식으로 데이터가 전달되는 경우가 많다
