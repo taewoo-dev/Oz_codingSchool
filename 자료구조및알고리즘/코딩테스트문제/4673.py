c7af722c8e354323694e49cb1712ecf51cb65d45
taewoo-dev <twin9774@gmail.com>
2024-08-08 14:38:26 +0900

    코딩테스트 문제

---
+++ 자료구조및알고리즘/코딩테스트문제/4673.py
@@ -0,0 +1,55 @@
+"""
+문제
+셀프 넘버는 1949년 인도 수학자 D.R. Kaprekar가 이름 붙였다. 양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수라고 정의하자.
+예를 들어, d(75) = 75+7+5 = 87이다.
+
+양의 정수 n이 주어졌을 때, 이 수를 시작해서 n, d(n), d(d(n)), d(d(d(n))), ...과 같은 무한 수열을 만들 수 있다. 
+
+예를 들어, 33으로 시작한다면 다음 수는 33 + 3 + 3 = 39이고, 그 다음 수는 39 + 3 + 9 = 51, 다음 수는 51 + 5 + 1 = 57이다. 이런식으로 다음과 같은 수열을 만들 수 있다.
+
+33, 39, 51, 57, 69, 84, 96, 111, 114, 120, 123, 129, 141, ...
+
+n을 d(n)의 생성자라고 한다. 위의 수열에서 33은 39의 생성자이고, 39는 51의 생성자, 51은 57의 생성자이다. 생성자가 한 개보다 많은 경우도 있다.
+예를 들어, 101은 생성자가 2개(91과 100) 있다. 
+
+생성자가 없는 숫자를 셀프 넘버라고 한다. 100보다 작은 셀프 넘버는 총 13개가 있다. 1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97
+
+10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+입력
+입력은 없다.
+
+출력
+10,000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 증가하는 순서로 출력한다.
+"""
+result_set = set()
+num_set = set(range(1,101))
+def func(n):
+    answer = n
+    num_1000,x = divmod(n,1000)
+    answer += num_1000
+    num_100,x = divmod(x,100)
+    answer += num_100
+    x,y = divmod(x,10)
+    answer = answer + x + y
+    return answer
+
+# 반복문을 돌면서 function 결과 값이 나오면 그 수를 self_num_dict에 추가한다.
+# 만약 self_num_dict에 이미 숫자가 있다면 그 값을 self_num_dict에 추가한다.
+for i in range(1,100):
+    result = func(i)
+
+    if result not in result_set:
+        result_set.add(result)
+
+self_num = list(num_set - result_set)
+self_num.sort()
+print(self_num)
+    
+        
+
+    
+# for i in self_num_dict:
+#     if self_num_dict[i] == True:
+#         print(i)
+
